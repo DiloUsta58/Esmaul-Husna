d628c3fee62f91e51bd3e2418c1bd353f7ea131e
DiloUsta58 <seven_seveni@hotmail.com>
2025-12-08 19:53:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "esma-husna-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./script/fs.js",
+  "./sounds/Esmaul-Husna.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
